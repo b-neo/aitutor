f239f2fc42d7f0182d1d21ecb91995a91fb9c130
b-neo <knb@laivdata.com>
2024-02-26 18:20:18 +0900

    Updates

---
+++ _next/static/9PvC--N3pVWiZJwXXaJVR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/447-9e181f9470382d5b.js","static/css/c66d02d86e111836.css","static/chunks/pages/index-6866378a70c77683.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- server/font-loader-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__FONT_LOADER_MANIFEST={pages:{"/_app":["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},app:{},appUsingSizeAdjust:!1,pagesUsingSizeAdjust:!0};
