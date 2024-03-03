8f503903401979b11dd31f110b933425b1318ed8
b-neo <choices@kakao.com>
2024-03-03 16:28:41 +0900

    asd

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -17,7 +17,7 @@ Standalone question:`;
 const QA_TEMPLATE = `You are an expert researcher. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-Say respond in Korean.
+You must say respond in Korean.
 
 <context>
   {context}
